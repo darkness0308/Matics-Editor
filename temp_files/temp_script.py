26f311f6e1598038df7c28aa46dbb186c5bc0c4a
Kamal M <133686913+darkness0308@users.noreply.github.com>
2025-02-21 06:07:28 +0530

    Add files via upload

---
+++ temp_files/temp_script.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+# Home route
+@app.route('/')
+def home():
+    return '<h1>Welcome to the Flask Web Application!</h1>'
+
+# Greeting route with user input
+@app.route('/greet', methods=['GET', 'POST'])
+def greet():
+    if request.method == 'POST':
+        name = request.form['name']
+        return f'<h1>Hello, {name}!</h1>'
+    return '''
+        <form method="post">
+            Name: <input type="text" name="name"><br>
+            <input type="submit" value="Greet">
+        </form>
+    '''
+
+if __name__ == '__main__':
+    app.run(debug=True)
